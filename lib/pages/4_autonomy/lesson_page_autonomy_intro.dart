85ce11c2af4451032ebdd5ed47dd6b95547f5b1c
sqrasdf <grw4zyyy@gmail.com>
2025-12-20 23:45:47 +0100

    add firebase access to module 4

--- lib/pages/4_autonomy/lesson_page_autonomy_intro.dart
+++ lib/pages/4_autonomy/lesson_page_autonomy_intro.dart
@@ -4,6 +4,7 @@ import 'package:autosense/lesson_widgets/main_text_widget.dart';
 import 'package:autosense/lesson_widgets/section_title.dart';
 import 'package:autosense/lesson_widgets/lesson_title_panel.dart';
 import 'package:autosense/lesson_widgets/text_icon_widget.dart';
+import 'package:autosense/utils.dart';
 import 'package:flutter/material.dart';
 
 class LessonPageAutonomyIntro extends StatefulWidget {
@@ -15,6 +16,49 @@ class LessonPageAutonomyIntro extends StatefulWidget {
 }
 
 class _LessonPageAutonomyLevels02State extends State<LessonPageAutonomyIntro> {
+  late ScrollController _scrollController;
+  bool _isLessonCompleted = false;
+  final double _completionThreshold = 0.90;
+
+  int lessonNumber = 11;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_checkScrollCompletion);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_checkScrollCompletion);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _checkScrollCompletion() async {
+    if (!_scrollController.hasClients) return;
+
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.offset;
+
+    if (maxScroll > 0) {
+      final double scrollProgress = currentScroll / maxScroll;
+
+      if (scrollProgress >= _completionThreshold && !_isLessonCompleted) {
+        setState(() {
+          _isLessonCompleted = true;
+        });
+
+        await saveLessonCompletionByIndex(lessonNumber - 1, 13);
+
+        _scrollController.removeListener(_checkScrollCompletion);
+
+        debugPrint('LEKCJA $lessonNumber UKOŃCZONA!');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     const double horizontalPadding = 25;
@@ -23,12 +67,13 @@ class _LessonPageAutonomyLevels02State extends State<LessonPageAutonomyIntro> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
         child: SingleChildScrollView(
+          controller: _scrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               LessonTitlePanel(
                 title: "Poziomy Autonomii SAE (0-2) - Asysta Kierowcy",
-                lessonNumber: 11,
+                lessonNumber: lessonNumber,
               ),
               const SizedBox(height: 5),
               const Divider(),
--- lib/pages/4_autonomy/lesson_page_levels_35.dart
+++ lib/pages/4_autonomy/lesson_page_levels_35.dart
@@ -4,6 +4,7 @@ import 'package:autosense/lesson_widgets/main_text_widget.dart';
 import 'package:autosense/lesson_widgets/section_title.dart';
 import 'package:autosense/lesson_widgets/lesson_title_panel.dart';
 import 'package:autosense/lesson_widgets/text_icon_widget.dart';
+import 'package:autosense/utils.dart';
 import 'package:flutter/material.dart';
 
 Widget _buildTableHeaderCell(String text) {
@@ -94,6 +95,49 @@ class _LessonPageLevels35State extends State<LessonPageLevels35> {
     );
   }
 
+  late ScrollController _scrollController;
+  bool _isLessonCompleted = false;
+  final double _completionThreshold = 0.90;
+
+  int lessonNumber = 12;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_checkScrollCompletion);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_checkScrollCompletion);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _checkScrollCompletion() async {
+    if (!_scrollController.hasClients) return;
+
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.offset;
+
+    if (maxScroll > 0) {
+      final double scrollProgress = currentScroll / maxScroll;
+
+      if (scrollProgress >= _completionThreshold && !_isLessonCompleted) {
+        setState(() {
+          _isLessonCompleted = true;
+        });
+
+        await saveLessonCompletionByIndex(lessonNumber - 1, 13);
+
+        _scrollController.removeListener(_checkScrollCompletion);
+
+        debugPrint('LEKCJA $lessonNumber UKOŃCZONA!');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     const double horizontalPadding = 25;
@@ -102,12 +146,13 @@ class _LessonPageLevels35State extends State<LessonPageLevels35> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
         child: SingleChildScrollView(
+          controller: _scrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               LessonTitlePanel(
                 title: "Poziomy 3-5: jazda autonomiczna i pełna autonomia",
-                lessonNumber: 12,
+                lessonNumber: lessonNumber,
               ),
               const SizedBox(height: 5),
               const Divider(),
