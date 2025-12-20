650de107db785d314756474fb361ee9d1cebfbd1
sqrasdf <grw4zyyy@gmail.com>
2025-12-20 23:31:39 +0100

    add firebase access to module 2

--- lib/pages/2_strategies/lesson_page_algorithms.dart
+++ lib/pages/2_strategies/lesson_page_algorithms.dart
@@ -6,6 +6,7 @@ import 'package:autosense/lesson_widgets/section_title.dart';
 import 'package:autosense/lesson_widgets/lesson_title_panel.dart';
 import 'package:autosense/lesson_widgets/text_icon_widget.dart';
 import 'package:autosense/lesson_widgets/bullet_point_lesson.dart';
+import 'package:autosense/utils.dart';
 import 'package:flutter/material.dart';
 
 class LessonPageAlgorithms extends StatefulWidget {
@@ -16,6 +17,49 @@ class LessonPageAlgorithms extends StatefulWidget {
 }
 
 class _LessonPageDecisionMakingState extends State<LessonPageAlgorithms> {
+  late ScrollController _scrollController;
+  bool _isLessonCompleted = false;
+  final double _completionThreshold = 0.90;
+
+  int lessonNumber = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_checkScrollCompletion);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_checkScrollCompletion);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _checkScrollCompletion() async {
+    if (!_scrollController.hasClients) return;
+
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.offset;
+
+    if (maxScroll > 0) {
+      final double scrollProgress = currentScroll / maxScroll;
+
+      if (scrollProgress >= _completionThreshold && !_isLessonCompleted) {
+        setState(() {
+          _isLessonCompleted = true;
+        });
+
+        await saveLessonCompletionByIndex(lessonNumber - 1, 13);
+
+        _scrollController.removeListener(_checkScrollCompletion);
+
+        debugPrint('LEKCJA $lessonNumber UKOŃCZONA!');
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     const double horizontalPadding = 25;
@@ -24,12 +68,13 @@ class _LessonPageDecisionMakingState extends State<LessonPageAlgorithms> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
         child: SingleChildScrollView(
+          controller: _scrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               LessonTitlePanel(
                 title: "Algorytmy Decyzyjne - Mózg Autonomii",
-                lessonNumber: 5,
+                lessonNumber: lessonNumber,
               ),
               const Divider(),
               const SizedBox(height: 10),
--- lib/pages/2_strategies/lesson_page_examples.dart
+++ lib/pages/2_strategies/lesson_page_examples.dart
@@ -4,6 +4,7 @@ import 'package:autosense/lesson_widgets/main_text_widget.dart';
 import 'package:autosense/lesson_widgets/section_title.dart';
 import 'package:autosense/lesson_widgets/lesson_title_panel.dart';
 import 'package:autosense/pages/2_strategies/pseudocode_widget.dart';
+import 'package:autosense/utils.dart';
 import 'package:flutter/material.dart';
 
 class LessonPageExamples extends StatefulWidget {
@@ -25,7 +26,48 @@ class SummaryRow {
 }
 
 class _LessonPageExamplesState extends State<LessonPageExamples> {
-  // Model dla wiersza w tabeli podsumowującej
+  late ScrollController _scrollController;
+  bool _isLessonCompleted = false;
+  final double _completionThreshold = 0.90;
+
+  int lessonNumber = 6;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_checkScrollCompletion);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_checkScrollCompletion);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _checkScrollCompletion() async {
+    if (!_scrollController.hasClients) return;
+
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.offset;
+
+    if (maxScroll > 0) {
+      final double scrollProgress = currentScroll / maxScroll;
+
+      if (scrollProgress >= _completionThreshold && !_isLessonCompleted) {
+        setState(() {
+          _isLessonCompleted = true;
+        });
+
+        await saveLessonCompletionByIndex(lessonNumber - 1, 13);
+
+        _scrollController.removeListener(_checkScrollCompletion);
+
+        debugPrint('LEKCJA $lessonNumber UKOŃCZONA!');
+      }
+    }
+  }
 
   final List<SummaryRow> summaryData = [
     SummaryRow(
@@ -53,12 +95,13 @@ class _LessonPageExamplesState extends State<LessonPageExamples> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
         child: SingleChildScrollView(
+          controller: _scrollController,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               LessonTitlePanel(
                 title: "Przykłady Zachowania Pojazdu Autonomicznego",
-                lessonNumber: 6,
+                lessonNumber: lessonNumber,
               ),
               const SizedBox(height: 5),
               const Divider(),
