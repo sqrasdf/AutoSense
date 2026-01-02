2934333647cf94ac7abdc2853b5a1614ec53781b
sqrasdf <grw4zyyy@gmail.com>
2026-01-02 21:36:08 +0100

    init 5 module

---
+++ lib/pages/5_quiz/lesson_page_final_quiz.dart
@@ -0,0 +1,81 @@
+import 'package:autosense/lesson_widgets/lesson_title_panel.dart';
+import 'package:autosense/utils.dart';
+import 'package:flutter/material.dart';
+
+class LessonPageFinalQuiz extends StatefulWidget {
+  const LessonPageFinalQuiz({super.key});
+
+  @override
+  State<LessonPageFinalQuiz> createState() => LessonPageFinalQuizState();
+}
+
+class LessonPageFinalQuizState extends State<LessonPageFinalQuiz> {
+  late ScrollController _scrollController;
+  bool _isLessonCompleted = false;
+  final double _completionThreshold = 0.90;
+
+  int lessonNumber = 14;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_checkScrollCompletion);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_checkScrollCompletion);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _checkScrollCompletion() async {
+    if (!_scrollController.hasClients) return;
+
+    final double maxScroll = _scrollController.position.maxScrollExtent;
+    final double currentScroll = _scrollController.offset;
+
+    if (maxScroll > 0) {
+      final double scrollProgress = currentScroll / maxScroll;
+
+      if (scrollProgress >= _completionThreshold && !_isLessonCompleted) {
+        setState(() {
+          _isLessonCompleted = true;
+        });
+
+        await saveLessonCompletionByIndex(lessonNumber - 1, 13);
+
+        _scrollController.removeListener(_checkScrollCompletion);
+
+        debugPrint('LEKCJA $lessonNumber UKOŃCZONA!');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double horizontalPadding = 25;
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: horizontalPadding),
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              LessonTitlePanel(
+                title: "Symulacja sytuacji drogowej",
+                lessonNumber: lessonNumber,
+              ),
+              const SizedBox(height: 5),
+              const Divider(),
+              const SizedBox(height: 10),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
